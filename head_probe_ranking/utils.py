836e4d29b96b0b86d9611415daad696c69e0cb69
Junhwi Kim <junhwi.kimsc@gmail.com>
2022-03-04 17:58:47 -0500

    dummy commit

--- head_probe_ranking/utils.py
+++ head_probe_ranking/utils.py
@@ -105,7 +105,7 @@ def plot_altair(dfs):
         for df in dfs:
             d.append(df.iloc[:, layer])
         d = pd.concat(d, axis=1)
-        d.index = [f'k={k}' for k in list(range(24, 144, 24))]
+        d.index = list(range(24, 144, 24))
         d.columns = columns
         layer_dfs.append(d)
     
@@ -119,7 +119,7 @@ def plot_altair(dfs):
     
     chart = alt.Chart(df).mark_bar().encode(
         # tell Altair which field to group columns on
-        x=alt.X('model:N', title=''),
+        x=alt.X('model:N'),
 
         # tell Altair which field to use as Y values and how to calculate
         y=alt.Y(
@@ -128,8 +128,7 @@ def plot_altair(dfs):
             scale=alt.Scale(domain=(0, 1))),
 
         # tell Altair which field to use to use as the set of columns to be  represented in each group
-        # hack: title is model so we can move it to the bottom later
-        column=alt.Column('k:O', title='model', sort=[f'k={k}' for k in list(range(24, 144, 24))]),
+        column=alt.Column('k:O', title='k'),
 
         # tell Altair which field to use for color segmentation 
         color=alt.Color('Layer:N', scale=alt.Scale(range=colors), sort=list(range(1, 13))[::-1]),
@@ -137,96 +136,9 @@ def plot_altair(dfs):
         order=alt.Order('Layer', sort='ascending')
     ).configure_view(
         strokeOpacity=0
-    ).configure_header(
-        titleOrient='bottom'
     )
 
     return chart
-
-def plot_seaborn(dfs):
-    for i, df in enumerate(dfs):
-        df["Name"] = f"df{i}"
-    dfall = pd.concat([pd.melt(i.reset_index(), id_vars=["Name", "index"]) for i in dfs], ignore_index=True)
-    dfall.set_index(["Name", "index", "variable"], inplace=True)
-    dfall["vcs"] = dfall.groupby(level=["Name", "index"]).cumsum()
-    dfall.reset_index(inplace=True) 
-
-    cs = [
-        "blue",
-        "orange",
-        "green",
-        "red",
-        "purple",
-        'brown',
-        'pink',
-        'gray',
-        'olive',
-        'cyan']
-    cs = [f'tab:{c}' for c in cs]
-    cs.extend(['k', 'w'])
-
-    for i, g in enumerate(dfall.groupby("variable")):
-        ax = sns.barplot(
-            data=g[1],
-            x="index",
-            y="vcs",
-            hue="Name",
-            color=cs[i],
-            zorder=-i, # so first bars stay on top
-            edgecolor="k")
-    ax.legend_.remove() # remove the redundant legends 
-    return ax
-
-def plot_clustered_stacked(dfall, labels=None, title="multiple stacked bar plot",  H="/", **kwargs):
-    """
-    Given a list of dataframes, with identical columns and index, create a clustered stacked bar plot. 
-    labels is a list of the names of the dataframe, used for the legend
-    title is a string for the title of the plot
-    H is the hatch used for identification of the different dataframe
-
-    For us, each dataframe needs to be:
-    different k's on the index
-    different layers on the columns
-    each dataframe represents different model
-
-    From: https://stackoverflow.com/questions/22787209/how-to-have-clusters-of-stacked-bars-with-python-pandas
-    """
-
-    n_df = len(dfall)
-    n_col = len(dfall[0].columns) 
-    n_ind = len(dfall[0].index)
-    axe = plt.subplot(111)
-
-    for df in dfall : # for each data frame
-        axe = df.plot(kind="bar",
-                      linewidth=0,
-                      stacked=True,
-                      ax=axe,
-                      legend=False,
-                      grid=False,
-                      **kwargs)  # make bar plots
-
-    h,l = axe.get_legend_handles_labels() # get the handles we want to modify
-    for i in range(0, n_df * n_col, n_col): # len(h) = n_col * n_df
-        for j, pa in enumerate(h[i:i+n_col]):
-            for rect in pa.patches: # for each index
-                rect.set_x(rect.get_x() + 1 / float(n_df + 1) * i / float(n_col))
-                rect.set_width(1 / float(n_df + 1))
-
-    axe.set_xticks((np.arange(0, 2 * n_ind, 2) + 1 / float(n_df + 1)) / 2.)
-    axe.set_xticklabels(df.index, rotation = 0)
-    axe.set_title(title)
-
-    # Add invisible data to add another legend
-    n=[]        
-    for i in range(n_df):
-        n.append(axe.bar(0, 0, color="gray", hatch=H * i))
-
-    l1 = axe.legend(h[:n_col], l[:n_col], loc=[1.01, 0.5])
-    if labels is not None:
-        l2 = plt.legend(n, labels, loc=[1.01, 0.1]) 
-    axe.add_artist(l1)
-    return axe
 
 if __name__ == '__main__':
     # rank_heads()
