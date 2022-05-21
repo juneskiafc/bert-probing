0a69d88750cd622deb28d255b9c69e29139b4e04
Junhwi Kim <junhwi.kimsc@gmail.com>
2022-05-21 14:21:01 -0400

    updates

--- model_probe.py
+++ model_probe.py
@@ -3,17 +3,18 @@ import argparse
 import subprocess
 from experiments.exp_def import (
     Experiment,
-    TaskDef,
     TaskDefs,
 )
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_ckpt', type=str, default='')
+parser.add_argument('--finetuned_task', type=str, default='')
+parser.add_argument('--finetuned_setting', type=str, default='')
 parser.add_argument('--task', type=str, default='')
 parser.add_argument('--devices', nargs='+')
 args = parser.parse_args()
 
-model_ckpt = Path(args.model_ckpt)
+model_ckpt_dir = Path('checkpoint').joinpath(f'{args.finetuned_task.upper()}_{args.finetuned_setting}')
+model_ckpt = list(model_ckpt_dir.rglob("model_5*.pt"))[0]
 devices = [int(d) for d in args.devices]
 
 processes = []
@@ -23,9 +24,9 @@ else:
     tasks = [Experiment[args.task.upper()]]
 
 for downstream_task in tasks:
-    exp_name = f'{model_ckpt.parent.name}:{downstream_task.name}'
+    exp_name = f'model_probing/{args.finetuned_task}/{args.finetuned_setting}/{downstream_task.name}'
     dataset = f'{downstream_task.name}/cross'
-    
+
     task_def_path = Path('experiments').joinpath(downstream_task.name, 'task_def.yaml')
     task_def = TaskDefs(task_def_path).get_task_def(downstream_task.name.lower())
 
