ee752a548b20907fd8ebfe8d899109f4dfa258d6
Junhwi Kim <junhwi.kimsc@gmail.com>
2022-05-21 15:23:07 -0400

    updates

--- model_probe.py
+++ model_probe.py
@@ -13,8 +13,12 @@ parser.add_argument('--task', type=str, default='')
 parser.add_argument('--devices', nargs='+')
 args = parser.parse_args()
 
-model_ckpt_dir = Path('checkpoint').joinpath(f'{args.finetuned_task.upper()}_{args.finetuned_setting}')
-model_ckpt = list(model_ckpt_dir.rglob("model_5*.pt"))[0]
+if args.finetuned_task != '':
+    model_ckpt_dir = Path('checkpoint').joinpath(f'{args.finetuned_task.upper()}_{args.finetuned_setting}')
+    model_ckpt = list(model_ckpt_dir.rglob("model_5*.pt"))[0]
+else:
+    model_ckpt = ''
+
 devices = [int(d) for d in args.devices]
 
 processes = []
@@ -24,9 +28,12 @@ else:
     tasks = [Experiment[args.task.upper()]]
 
 for downstream_task in tasks:
-    exp_name = f'model_probing/{args.finetuned_task}/{args.finetuned_setting}/{downstream_task.name}'
+    if args.finetuned_task != '':
+        exp_name = f'model_probing/{args.finetuned_task}/{args.finetuned_setting}/{downstream_task.name}'
+    else:
+        exp_name = f'model_probing/BERT/{downstream_task.name}'
+    
     dataset = f'{downstream_task.name}/cross'
-
     task_def_path = Path('experiments').joinpath(downstream_task.name, 'task_def.yaml')
     task_def = TaskDefs(task_def_path).get_task_def(downstream_task.name.lower())
 
@@ -36,7 +43,9 @@ for downstream_task in tasks:
     cmd += f' --exp_name {exp_name}'
     cmd += f' --dataset_name {dataset}'
     cmd += ' --epochs 2'
-    cmd += f' --model_ckpt {model_ckpt} --resume'
+
+    if model_ckpt != '':
+        cmd += f' --model_ckpt {model_ckpt} --resume'
 
     if downstream_task in [Experiment.NER, Experiment.POS]:
         cmd += ' --model_probe_sequence'
